9d7277243714d187ddc40b5fbed85b649d3edecd
Pascal <posixpascal@googlemail.com>
2022-10-23 14:29:02 +0200

    fix: split private/public rooms

--- server/src/app.ts
+++ server/src/app.ts
@@ -19,7 +19,8 @@ app.get('/', (req: express.Request, res: express.Response) => {
   res.send('hello world');
 });
 
-const rooms: Record<string, Room> = {};
+const privateRooms: Record<string, Room> = {};
+const publicRooms: Record<string, Room> = {};
 
 io.on('connection', (socket: Socket) => {
   console.log('a user connected: ', socket.id);
@@ -28,21 +29,45 @@ io.on('connection', (socket: Socket) => {
     console.log('a user disconnected');
   });
 
-  const createRoom = (roomName: string = '') => {
-    let roomId = roomName || nanoid(5);
-    while (rooms[roomId]) {
-      roomId = nanoid(5);
+  const createPublicRoom = () => {
+    let roomName = nanoid(5);
+    while (publicRooms[roomName]) {
+      roomName = nanoid(5);
     }
 
-    const room = new Room(roomId);
+    const room = new Room(roomName);
     room.join(socket);
+    publicRooms[roomName] = room;
+  };
 
-    rooms[roomId] = room;
+  const createPrivateRoom = (roomName: string) => {
+    if (privateRooms[roomName]) {
+      socket.emit('roomUnavailable', roomName);
+      return;
+    }
+
+    const room = new Room(roomName);
+    room.join(socket);
+    privateRooms[roomName] = room;
+  };
+
+  const findFreeRoom = () => {
+    for (const roomId in publicRooms) {
+      const room = publicRooms[roomId];
+
+      if (room.playerCount < 2) {
+        room.join(socket);
+        return;
+      }
+    }
+
+    // no room was present, lets create a new one
+    createPublicRoom();
   };
 
   const joinRoom = (roomId: string) => {
     console.log('attempt to join room: ', roomId);
-    const room = rooms[roomId];
+    const room = publicRooms[roomId] || privateRooms[roomId];
     if (!room) {
       socket.emit('roomNotFound', roomId);
       return;
@@ -60,7 +85,7 @@ io.on('connection', (socket: Socket) => {
     event: string;
     data: any;
   }) => {
-    const room = rooms[to];
+    const room = publicRooms[to] || privateRooms[to];
     if (!room) {
       return;
     }
@@ -68,27 +93,14 @@ io.on('connection', (socket: Socket) => {
     room.onMessage(socket, event, data);
   };
 
-  socket.on('findFreeRoom', () => {
-    for (const roomId in rooms) {
-      const room = rooms[roomId];
-
-      if (room.playerCount < 2) {
-        room.join(socket);
-        return;
-      }
-    }
-
-    // no room was present, lets create a new one
-    createRoom();
-  });
-
-  socket.on('createRoom', createRoom);
+  socket.on('findFreeRoom', findFreeRoom);
+  socket.on('createRoom', createPrivateRoom);
   socket.on('joinRoom', joinRoom);
   socket.on('message', message);
 
   socket.on('leaveRoom', (roomId: string) => {
     console.log('leave room', roomId);
-    const room = rooms[roomId];
+    const room = publicRooms[roomId] || privateRooms[roomId];
     if (!room) {
       return;
     }
